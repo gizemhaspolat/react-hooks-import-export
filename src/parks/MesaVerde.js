fc7561da6f55077c4a1e2ef503b79663ae2ff999
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-06-20 13:02:49 +0300

    completed assignment

--- src/parks/MesaVerde.js
+++ src/parks/MesaVerde.js
@@ -1,7 +1,9 @@
 import React from "react";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function MesaVerde() {
   return <h1>Mesa Verde National Park</h1>;
 }
+RMFunctions.wildlife();
 
 export default MesaVerde;
